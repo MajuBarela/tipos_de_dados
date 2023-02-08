a6ad2489b5424fd5a49a371627f840b1015e1f35
MajuBarela <maria.netto01@etec.sp.gov.br>
2023-02-08 17:04:13 +0000

    variaveis salvas

--- Variaveis.java
+++ Variaveis.java
@@ -1,4 +1,29 @@
 public class Variaveis {
+    public static void main(String[] args) {
 
-    
+        int idade = 39;
+        byte quantidadeBolas = 7;
+        char letra = 'M';
+        boolean vivo = false;
+        double salario = 1302.00;
+        float salarioMenor = 1212.00f;
+        short alunosPalmital = 10000;
+        long populacaoPalmital = 22000;
+
+        String nome;
+        nome = "Barela da Etec Palmital";
+
+        System.out.println("Meu nome é: " + nome + ", e tenho: " + idade + " anos");
+        System.out.println(nome);
+        System.out.println(idade);
+
+        System.out.println(quantidadeBolas);
+        System.out.println(letra);
+        System.out.println(vivo);
+        System.out.println(salario);
+        System.out.println(salarioMenor);
+        System.out.println(alunosPalmital);
+        System.out.println(populacaoPalmital);
+    }
+
 }
