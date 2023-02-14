0039c2d242ef6c95a92f59a2f31a115bc62e53bc
MajuBarela <maria.netto01@etec.sp.gov.br>
2023-02-14 14:12:22 +0000

    salvando operadores

--- Operadores.java
+++ Operadores.java
@@ -1,4 +1,13 @@
 public class Operadores {
+    public static void main (String[] barela){
 
-    
+        int idade;
+        idade = 16;
+        idade++;
+        idade++;
+        System.out.println("Eu tenho " + idade + " anos");
+        
+        System.out.println(idade);
+    }
+
 }
