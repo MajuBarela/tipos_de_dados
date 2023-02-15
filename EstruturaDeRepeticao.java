cf867e54167d602f218463cbad1bc01b7a2df645
MajuBarela <maria.netto01@etec.sp.gov.br>
2023-02-15 17:11:09 +0000

    salvando exercícios

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,3 +1,27 @@
 public class EstruturaDeRepeticao {
     
+    public static void main(String args[]){
+
+        /*Primeiro exercício concluído 
+        for(int i=150; i<=300; i++){
+            System.out.println(i);
+        } */
+
+        /*Segundo exercício concluído 
+        int soma = 0;
+        for(int i=1; i<=1000; i++){
+            soma = soma + i;
+        } 
+        System.out.println(soma); */
+
+        /*Terceiro exercício concluído 
+        for(int i=3; i<100; i+=3){
+            System.out.println(i);
+        } */
+
+        int fatResult = 0;
+        for (int n=1; n<=10; n++){
+            
+        }
+    }
 }
